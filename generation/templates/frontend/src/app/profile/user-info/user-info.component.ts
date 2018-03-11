58ad8bc47c5225fc077932dc13f9ad16b88a670f
Aleksandar Kahriman <akahriman@execom.eu>
2018-03-11 14:22:51 +0100

    User profile done.

--- generation/templates/frontend/src/app/profile/user-info/user-info.component.ts
+++ generation/templates/frontend/src/app/profile/user-info/user-info.component.ts
@@ -1,8 +1,31 @@
-import { Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../user/shared/user.service';
+import {User} from '../../user/shared/user.model';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.css']
 })
-export class UserInfoComponent { }
+export class UserInfoComponent implements OnInit {
+
+  user: User;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+
+    this.userService.getUserData().subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  get name() {
+    return this.user.firstName + ' ' + this.user.lastName;
+  }
+
+  get email() {
+    return this.user.email;
+  }
+}
