bc946f7880ee57b4b920057ea36de0234eb088b9
Milorad Vojnović <milekuglas@gmail.com>
2018-04-14 15:31:18 +0200

    Implement support for package and jwt declaration in XMI file
    Closes #43

--- generation/generators/dsl_generator.py
+++ generation/generators/dsl_generator.py
@@ -19,52 +19,65 @@ class DslGenerator(Generator):
         stereotypes_names = []
         stereotypes = []
         enums = []
+        package_name = None
+        jwt_secret = None
 
         generated = []
 
-        for child in tree_root:
-            if child.tag.endswith("Model"):
-                for ch in child:
-                    if ch.tag == "packagedElement" and ch.get("name") != "str":
-                        if ch.get("{http://www.omg.org/spec/XMI/20131001}type") == "uml:Class":
-                            attributes = []
-                            generalization = {}
-                            for child in ch:
-                                if child.tag == "ownedAttribute":
-                                    try:
-                                        att_type = child[0][0][0].get("referentPath")
-                                        temp = att_type.split(":")
-                                        attribute = {"name": child.get("name"), "type": temp[len(temp) - 1]}
-                                        attributes.append(attribute)
-                                    except IndexError:
-                                        if child.get("name") is not None and child.get("type") is not None:
-                                            attribute = {"name": child.get("name"), "type": "enum" + child.get("type")}
-                                            attributes.append(attribute)
-                                        else:
-                                            continue
+        for c in tree_root:
+            if c.tag.endswith("Model"):
+                for child in c:
+                    if child.get("{http://www.omg.org/spec/XMI/20131001}type") == "uml:Package":
+                        package_name = child.get("name")
+                        for ch in child:
+                            if ch.tag == "packagedElement" and ch.get("name") != "str":
+                                if ch.get("{http://www.omg.org/spec/XMI/20131001}type") == "uml:Class":
+                                    attributes = []
+                                    generalization = {}
+                                    for att in ch:
+                                        if att.tag == "ownedAttribute":
+                                            try:
+                                                att_type = att[0][0][0].get("referentPath")
+                                                temp = att_type.split(":")
+                                                attribute = {"name": att.get("name"), "type": temp[len(temp) - 1]}
+                                                attributes.append(attribute)
+                                            except IndexError:
+                                                if att.get("name") is not None and att.get("type") is not None:
+                                                    attribute = {"name": att.get("name"),
+                                                                 "type": "enum" + att.get("type")}
+                                                    attributes.append(attribute)
+                                                else:
+                                                    continue
 
-                                if child.tag == "generalization":
-                                    generalization = child.get("general")
-                            classes.append(
-                                {"name": ch.get("name"), "id": ch.get("{http://www.omg.org/spec/XMI/20131001}id"),
-                                 "attributes": attributes, "generalization": generalization})
-                        if ch.get("{http://www.omg.org/spec/XMI/20131001}type") == "uml:Stereotype":
-                            stereotypes_names.append(ch.get("name"))
-                        if ch.get("{http://www.omg.org/spec/XMI/20131001}type") == "uml:Enumeration":
-                            attributes = []
-                            for child in ch:
-                                if child.tag == "ownedLiteral":
-                                    attribute = child.get("name")
-                                    attributes.append(attribute)
-                            enums.append(
-                                {"name": ch.get("name"), "id": ch.get("{http://www.omg.org/spec/XMI/20131001}id"),
-                                 "attributes": attributes})
+                                        if att.tag == "generalization":
+                                            generalization = att.get("general")
+                                    classes.append(
+                                        {"name": ch.get("name"),
+                                         "id": ch.get("{http://www.omg.org/spec/XMI/20131001}id"),
+                                         "attributes": attributes, "generalization": generalization})
+                                if ch.get("{http://www.omg.org/spec/XMI/20131001}type") == "uml:Enumeration":
+                                    attributes = []
+                                    for lit in ch:
+                                        if lit.tag == "ownedLiteral":
+                                            attribute = lit.get("name")
+                                            attributes.append(attribute)
+                                    enums.append(
+                                        {"name": ch.get("name"),
+                                         "id": ch.get("{http://www.omg.org/spec/XMI/20131001}id"),
+                                         "attributes": attributes})
+                    if child.get("{http://www.omg.org/spec/XMI/20131001}type") == "uml:Profile":
+                        for ch in child:
+                            if ch.get("{http://www.omg.org/spec/XMI/20131001}type") == "uml:Stereotype":
+                                stereotypes_names.append(ch.get("name"))
 
         for child in tree_root:
             for stereotype_name in stereotypes_names:
                 if child.tag.endswith(stereotype_name):
-                    stereotype = {"name": stereotype_name, "class_id": child.get("base_Class")}
-                    stereotypes.append(stereotype)
+                    if child.tag.endswith("WebShopPackage"):
+                        jwt_secret = child.get("jwtSecret")
+                    else:
+                        stereotype = {"name": stereotype_name, "class_id": child.get("base_Class")}
+                        stereotypes.append(stereotype)
 
         for cl in classes:
             attributes = []
@@ -104,12 +117,11 @@ class DslGenerator(Generator):
         template = env.get_template("xmi/model.scan")
         rendered = template.render(
             {"base_product": base_product, "products": products, "root_categories": root_categories,
-             "mid_categories": mid_categories, "product_categories": product_categories, "enums": enums})
+             "mid_categories": mid_categories, "product_categories": product_categories, "enums": enums,
+             "package_name": package_name, "jwt_secret": jwt_secret})
         path = os.path.join(root, "metamodel")
         if not os.path.exists(path):
             os.makedirs(path)
         file_name = os.path.join(path, "model.scan")
         with open(file_name, "w+") as f:
             f.write(rendered)
-
-
